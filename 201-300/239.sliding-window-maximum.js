7760f70b1d6249d9e01d2ca922f9190c386281c1
Guangxu Li <guangxu.li@nyu.edu>
2021-02-25 23:27:33 -0500

    feat: +1 problem solved on Feb 25, 2021

---
+++ 201-300/239.sliding-window-maximum.js
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=239 lang=javascript
+ *
+ * [239] Sliding Window Maximum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+    const left = [];
+    const right = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        const j = nums.length - 1 - i;
+
+        if (i % k == 0)
+            left[i] = nums[i];
+        else
+            left[i] = Math.max(left[i - 1], nums[i]);
+
+        if ((j + 1) % k == 0)
+            right[j] = nums[j];
+        else
+            right[j] = Math.max(right[j + 1], nums[j]);
+    }
+
+    const maximum = [];
+    for (let i = 0; i <= nums.length - k; i++) {
+        maximum.push(Math.max(left[i + k - 1], right[i]));
+    }
+
+    return maximum;
+};
+// @lc code=end
+
